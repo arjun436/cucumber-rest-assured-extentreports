36b0184e0bcb1f205bb2d968b5714f1c4e66775a
arjun436 <arjun.miryala@hotmail.com>
2019-07-28 20:38:16 +0530

    updated tags

--- src/test/java/com/factory/cucumber/runner/RunCucumberTest.java
+++ src/test/java/com/factory/cucumber/runner/RunCucumberTest.java
@@ -12,7 +12,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(format = { "pretty" }, features = "src//test//java//com//factory//cucumber//features", glue = {
-		"com.factory.cucumber.stepdefinitions" }, tags = { "@hooks,@get,@post,@put,@getfail,@postfail,@putfail" }, plugin = {
+		"com.factory.cucumber.stepdefinitions" }, tags = { "@smoke, @hooks, @get, @post, @put, @getfail, @postfail, @putfail" }, plugin = {
 				"com.cucumber.listener.ExtentCucumberFormatter:src/test/reports/cucumber_report.html",
 				"html:output/html-report" }, monochrome = true)
 public class RunCucumberTest {
